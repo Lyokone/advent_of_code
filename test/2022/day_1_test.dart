4d7c7142a451c7153f879b2c57070144fc3718d9
Guillaume Bernos <guillaume@bernos.dev>
2022-12-01 10:12:19 +0100

    feat: big input in test

--- test/2022/day_1_test.dart
+++ test/2022/day_1_test.dart
@@ -1,6 +1,8 @@
 import 'package:advent_of_code/2022/day_1.dart';
 import 'package:test/test.dart';
 
+import 'day_1_input.dart';
+
 void main() {
   test('Small Input', () async {
     Day1 day = Day1('''
@@ -21,5 +23,11 @@ void main() {
 ''');
     expect(day.solvePartOne(), equals('24000'));
     expect(day.solvePartTwo(), equals('45000'));
+  });
+
+  test('Big Input', () async {
+    Day1 day = Day1(day_1_input);
+    print("Part 1 solution: ${day.solvePartOne()}");
+    print("Part 2 solution: ${day.solvePartTwo()}");
   });
 }
